f9d1603ee95f44c72d17e9120e0d771c55f99791
SMcGrath714277 <714277@student.dusd.net>
2024-01-26 12:36:58 -0800

    Completed MethodOverloading
    title.

---
+++ ScriptingProjectMcGrathP4/Assets/NewScripts/SomeClass.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public string Add(string str1, string str2)
+    {
+        return str1 + str2;
+    }
+}
