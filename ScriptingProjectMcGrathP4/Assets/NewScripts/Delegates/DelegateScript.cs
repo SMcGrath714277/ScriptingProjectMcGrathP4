cae83827d99511e1e0665b163ceef9184bbe0af4
SMcGrath714277 <714277@student.dusd.net>
2024-03-01 13:29:12 -0800

    Update
    Completed delegates.

---
+++ ScriptingProjectMcGrathP4/Assets/NewScripts/Delegates/DelegateScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateScript : MonoBehaviour
+{
+    delegate void MyDelegate(int num);
+    MyDelegate myDelegate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myDelegate = PrintNum;
+        myDelegate(50);
+
+        myDelegate = DoubleNum;
+        myDelegate(50);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PrintNum(int num)
+    {
+        print("Print Num: " + num * 2);
+    }
+
+    void DoubleNum(int num)
+    {
+        print ("Double Num: " + num * 2);
+    }
+}
---
+++ ScriptingProjectMcGrathP4/Assets/NewScripts/Delegates/MulticastScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MulticastScript : MonoBehaviour
+{
+    delegate void MultiDelegate();
+    MultiDelegate myMultiDelegate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMultiDelegate += PowerUp;
+        myMultiDelegate += TurnRed;
+
+        if(myMultiDelegate != null)
+        {
+            myMultiDelegate();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PowerUp()
+    {
+        print("Orb is powering up!");
+    }
+
+    void TurnRed()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
